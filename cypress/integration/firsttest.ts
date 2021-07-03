2d3d4d82571c381b4b983838dba64af8cb21ea87
ExecuteAutomation <kartmcad@gmail.com>
2021-07-03 15:53:16 +1200

    modified the files for tests

--- cypress/integration/firsttest.ts
+++ cypress/integration/firsttest.ts
@@ -6,27 +6,8 @@ describe("First test", () => {
         cy.visit("https://executeautomation.com");
     })
 
-    it("XHR Testing", () => {
-
-        //Simple
-        // cy.wait("@homepage").its('response.statusCode').should('be.oneOf', [200, 304])
-
-
-        //Advanced
-        cy.wait("@homepage").then(({ request, response }) => {
-            expect(response.body.courses).length.above(2);
-            expect(response.body.courses[0]).to.have.property('title', 'Selenium Grid with Docker');
-        })
-    })
-
-
-
-
     /* ==== Test Created with Cypress Studio ==== */
-    it('login', function() {
-
-
-
+    it('login', function () {
         /* ==== Generated with Cypress Studio ==== */
         cy.get('.ct-header-content > .ct-ul > :nth-child(3) > a').click();
         cy.get('#email').clear().type('karthik@techgeek.co.in');
@@ -34,7 +15,7 @@ describe("First test", () => {
         cy.get('#password').type('Projecta12');
         cy.get('.ct-btn-blue').click();
 
-        
+
         /* ==== End Cypress Studio ==== */
         /* ==== Generated with Cypress Studio ==== */
         cy.get('.btn > .svg-inline--fa').click();
